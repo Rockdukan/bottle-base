c418ec8d18021da40175685e49d05d6848a021cc
Rockdukan <i.makrianskii@rambler.ru>
2025-05-22 21:39:19 +0300

    Добавлено исправление кодировки в формах.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,25 @@
-from bottle import Bottle, TEMPLATE_PATH
+from bottle import Bottle, FormsDict, TEMPLATE_PATH
 
 from config import TEMPLATE_DIR
 
+
+# Исправляем декодирование
+def fix_encoding(value):
+    if isinstance(value, str):
+        try:
+            return value.encode('latin-1').decode('utf-8')
+        except (UnicodeDecodeError, UnicodeEncodeError):
+            return value
+    return value
+
+# Патчим метод получения значений из форм
+original_get = FormsDict.get
+
+def patched_get(self, key, default=None):
+    value = original_get(self, key, default)
+    return fix_encoding(value)
+
+FormsDict.get = patched_get
 
 app = Bottle()
 
