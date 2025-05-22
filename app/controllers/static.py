00b0e88bb0dc815422274c5c07140ddc4abb60f3
Rockdukan <i.makrianskii@rambler.ru>
2025-05-22 21:36:32 +0300

    Добавлен каталог media. Пути берутся из config.py

--- app/controllers/static.py
+++ app/controllers/static.py
@@ -1,23 +1,29 @@
 from bottle import static_file
 
 from app import app
+from config import MEDIA_DIR, STATIC_DIR
+
+
+@app.get(r"/media/<filepath:path>")
+def media(filepath):
+    return static_file(filepath, root=MEDIA_DIR)
 
 
 @app.get(r"/static/css/<filepath:re:.*\.css>")
 def css(filepath):
-    return static_file(filepath, root="app/static/css")
+    return static_file(filepath, root=os.path.join(STATIC_DIR, "css"))
 
 
 @app.get(r"/static/fonts/<filepath:re:.*\.(eot|otf|svg|ttf|woff|woff2?)>")
 def font(filepath):
-    return static_file(filepath, root="app/static/fonts")
+    return static_file(filepath, root=os.path.join(STATIC_DIR, "fonts"))
 
 
 @app.get(r"/static/img/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
 def img(filepath):
-    return static_file(filepath, root="app/static/img")
+    return static_file(filepath, root=os.path.join(STATIC_DIR, "img"))
 
 
 @app.get(r"/static/js/<filepath:re:.*\.(js|json)>")
 def js(filepath):
-    return static_file(filepath, root="app/static/js")
+    return static_file(filepath, root=os.path.join(STATIC_DIR, "js"))
