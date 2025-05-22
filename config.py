6fe380cbf9af45e20e921ff82445ab93b1a41258
Rockdukan <i.makrianskii@rambler.ru>
2025-05-22 21:40:30 +0300

    Добавлены настройки MEDIA_DIR и STATIC_DIR.

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MEDIA_DIR = os.path.join(BASE_DIR, "media")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 LOG_DIR = os.path.join(BASE_DIR, "logs")
 LOG_FILE = os.path.join(LOG_DIR, "app.log")
